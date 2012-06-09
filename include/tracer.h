c9305009a6e324723faae26cf21b3e11cbeeb6f9
Alex Horn <alex.horn@cs.ox.ac.uk>
2012-06-09 20:49:21 +0100

    Add open-source 3-clause BSD license terms.

--- include/tracer.h
+++ include/tracer.h
@@ -1,3 +1,7 @@
+// Copyright 2012, Alex Horn. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 #ifndef TRACER_H_
 #define TRACER_H_
 
