9cf71ca5bf879c754d50f0b85bdc325b86fa5614
Alex Horn <alex.horn@cs.ox.ac.uk>
2013-01-25 20:25:30 +0000

    Create reusable interpreter

--- include/interpreter.h
+++ include/interpreter.h
@@ -7,155 +7,157 @@
 
 #include <stdexcept>
 #include "expr.h"
+#include "eval.h"
 
 namespace se {
 
 /*! \file interpreter.h */
 
-/// Calls a member function through a member function pointer
+template<typename T, Operator op>
+class __Operator {
+protected:
+  static T eval(const T& larg, const T& rarg) {
+    return Eval<op>::eval(larg, rarg, larg);
+  }
 
-/// Example: CALL_MEMBER_FN(this, foo)(7) calls this->foo(7)
-#define CALL_MEMBER_FN(object_ptr,member_fn_ptr) ((object_ptr)->*(member_fn_ptr))
+  static T eval(const T& arg) {
+    return Eval<op>::eval(arg, arg);
+  }
+};
+
+template<typename T, Operator op>
+class __UnaryOperator : __Operator<T, op> {
+public:
+  static T build(Visitor<T> * const visitor,
+                 const SharedExpr& expr) {
+
+    return eval(expr->walk(visitor));
+  }
+};
+
+template<typename T, Operator op>
+class __BinaryOperator : __Operator<T, op> {
+public:
+  static T build(Visitor<T> * const visitor,
+                 const SharedExpr& lexpr, const SharedExpr& rexpr) {
+
+    return eval(lexpr->walk(visitor), rexpr->walk(visitor));
+  }
+};
+
+template<typename T, Operator op>
+class __NaryOperator : __Operator<T, op> {
+public:
+  static T build(Visitor<T> * const visitor,
+                 const T& identity, const std::list<SharedExpr>& exprs) {
+    T formula = identity;
+
+    for(SharedExpr expr : exprs) {
+      formula = eval(formula, expr->walk(visitor));
+    }
+
+    return formula;
+  }
+};
 
 /// Unsupported built-in C++ operation or primitive type
 typedef std::runtime_error InterpreterException;
 
-/// \interface Interpreter
-/// \brief Symbolic expression evaluator
+/// Symbolic expression interpreter
 
-/// Evaluates a syntactic expression through a DAG postorder traversal.
-/// Thus, an Interpreter gives semantics to a symbolic expression.
+/// Interprets a symbolic expression through the postorder traversal of its DAG.
+/// This way an Interpreter gives semantics to a symbolic expression.
 ///
 /// When the interpreter cannot evaluate an expression, it should throw
 /// an InterpreterException with a helpful human-readable error message.
 template<typename T>
 class Interpreter : public Visitor<T> {
+private:
+  typedef Visitor<T> * const Self;
+
+public:
+  typedef T (*UnaryOperator)(Self, const SharedExpr&);
+  typedef T (*BinaryOperator)(Self, const SharedExpr&, const SharedExpr&);
+  typedef T (*NaryOperator)(Self, const T& identity, const std::list<SharedExpr>&);
+
+private:
+  /* CAUTION: These three arrays depend on the op.h header! */
+
+  // The order of the unary operators must be the same as in the Operator enum
+  // data structure. Given such an enum value OP, the appropriate function
+  // pointer can be retrieved with m_unary_operators[OP - UNARY_BEGIN]. The
+  // size of the array is equal to (UNARY_END - UNARY_BEGIN) + 1.
+  static const UnaryOperator m_unary_operators[];
+
+  // The order of the binary operators must be the same as in the Operator enum
+  // data structure. Given such an enum value OP, the appropriate function
+  // pointer can be retrieved with m_binary_operators[OP - UNARY_END]. The size
+  // of the array is equal to (BINARY_END - NARY_BEGIN) + 1.
+  static const BinaryOperator m_binary_operators[];
+
+  // An array of associative binary operators. The order of these must be the
+  // same as in the Operator enum data structure. Given such an enum value OP,
+  // the appropriate function pointer is at m_nary_operators[OP - NARY_BEGIN].
+  // The size of the array is equal to (NARY_END - NARY_BEGIN) + 1.
+  static const NaryOperator m_nary_operators[];
+
+protected:
+  static UnaryOperator find_unary_operator(Operator op) {
+    return m_unary_operators[op - UNARY_BEGIN];
+  }
+
+  static BinaryOperator find_binary_operator(Operator op) {
+    return m_binary_operators[op - UNARY_END];
+  }
+
+  static NaryOperator find_nary_operator(Operator op) {
+    return m_nary_operators[op - NARY_BEGIN];
+  }
+
 public:
   virtual ~Interpreter() {}
 };
+
+template<typename T>
+const typename Interpreter<T>::
+  UnaryOperator Interpreter<T>::m_unary_operators[] =
+                        { &__UnaryOperator<T, NOT>::build };
+
+template<typename T>
+const typename Interpreter<T>::
+  BinaryOperator Interpreter<T>::m_binary_operators[] =
+                        { &__BinaryOperator<T, ADD>::build,
+                          &__BinaryOperator<T, LAND>::build,
+                          &__BinaryOperator<T, LOR>::build,
+                          &__BinaryOperator<T, EQL>::build,
+                          &__BinaryOperator<T, LSS>::build };
+
+template<typename T>
+const typename Interpreter<T>::
+  NaryOperator Interpreter<T>::m_nary_operators[] =
+                        { &__NaryOperator<T, ADD>::build,
+                          &__NaryOperator<T, LAND>::build,
+                          &__NaryOperator<T, LOR>::build };
 
 /// Strongest postcondition predicate transformer
 
 /// Logical statements about an evaluated expression can be proved using the
 /// Z3 theorem prover.
 class SpInterpreter : public Interpreter<z3::expr> {
-
-private:
-
-  // Internal member function pointer for fast dispatching of a unary operator.
-  typedef z3::expr (SpInterpreter::*UnaryOperator)(const SharedExpr&);
-
-  // Internal member function pointer for fast dispatching of a binary operator.
-  typedef z3::expr (SpInterpreter::*BinaryOperator)(const SharedExpr&, const SharedExpr&);
-
-  // Internal member function pointer for fast dispatching of an associative nary operator.
-  typedef z3::expr (SpInterpreter::*NaryOperator)(const std::list<SharedExpr>&);
-
-  /* CAUTION: The size of the following three array *
-   * fields depends on the number of operators!     */
-
-  // The order of the unary operators must be the same as in the Operator enum
-  // data structure. Given such an enum value OP, its member function pointer
-  // can be retrieved with m_unary_operators[OP - UNARY_BEGIN]. The size of the
-  // array must be equal to the number of times the UNARY_OP_DEF macro is
-  // evaluated inside this class.
-  const UnaryOperator m_unary_operators[1 /* see CAUTION note! */];
-
-  // The order of the binary operators must be the same as in the Operator enum
-  // data structure. Given such an enum value OP, its member function pointer
-  // can be retrieved with m_binary_operators[OP - UNARY_END]. The size of the
-  // array must be equal to the number of times the BINARY_OP_DEF macro is
-  // evaluated inside this class.
-  const BinaryOperator m_binary_operators[5 /* see CAUTION note! */];
-
-  // An array of associative binary operators. The order of these must be the
-  // same as in the Operator enum data structure. Given such an enum value OP,
-  // its member function pointer can be retrieved with
-  // m_nary_operators[OP - NARY_BEGIN]. The size of the array must be equal to
-  // the number of member function definitions of type NaryOperator.
-  const NaryOperator m_nary_operators[3 /* see CAUTION note! */];
-
-
-/// Defines a member function of type UnaryOperator
-
-/// The number of times this macro is called inside this class definition must
-/// be equal to the static size of the internal m_unary_operators array.
-#define UNARY_OP_DEF(op, op_name)\
-  z3::expr op_name(const SharedExpr& expr) {\
-    return op expr->walk(this);\
-  }
-
-UNARY_OP_DEF(!, NOT_1)
-
-/// Defines a member function of type BinaryOperator
-
-/// The number of times this macro is called inside this class definition must
-/// be equal to the static size of the internal m_binary_operators array.
-#define BINARY_OP_DEF(op, op_name)\
-  z3::expr op_name(const SharedExpr& x_expr, const SharedExpr& y_expr) {\
-    return x_expr->walk(this) op y_expr->walk(this);\
-  }
-
-BINARY_OP_DEF(+, ADD_2)
-BINARY_OP_DEF(&&, LAND_2)
-BINARY_OP_DEF(||, LOR_2)
-BINARY_OP_DEF(==, EQL_2)
-BINARY_OP_DEF(<, LSS_2)
-
-
-  // ADD_N defines a member function of type NaryOperator that sums up an
-  // arbitrary number of integer expressions. There must be at least two
-  // summands.
-  z3::expr ADD_N(const std::list<SharedExpr>& exprs) {
-    // Zero is the additive identity element
-    z3::expr z3_expr = context.int_val(0);
-
-    //TODO: Use Z3_mk_add
-    for(SharedExpr expr : exprs) {
-      z3_expr = z3_expr + expr->walk(this);
-    }
-
-    return z3_expr;
-  }
-
-  z3::expr LAND_N(const std::list<SharedExpr>& exprs) {
-    // True is the identity element for logical AND
-    z3::expr z3_expr = context.bool_val(true);
-
-    //TODO: Use Z3_mk_and
-    for(SharedExpr expr : exprs) {
-      z3_expr = z3_expr && expr->walk(this);
-    }
-
-    return z3_expr;
-  }
-
-
-  z3::expr LOR_N(const std::list<SharedExpr>& exprs) {
-    // False is the identity element for logical OR
-    z3::expr z3_expr = context.bool_val(false);
-
-    //TODO: Use Z3_mk_or
-    for(SharedExpr expr : exprs) {
-      z3_expr = z3_expr || expr->walk(this);
-    }
-
-    return z3_expr;
-  }
-
 public:
-
-  // C++ API to Z3 Theorem Prover
+  /* Order of fields must match the initialization order! */
   z3::context context;
   z3::solver solver;
   
+private:
+  z3::expr identities[3];
+
+public:
   SpInterpreter() : context(), solver(context),
-    m_unary_operators{&SpInterpreter::NOT_1},
-    m_binary_operators{&SpInterpreter::ADD_2, &SpInterpreter::LAND_2,
-                     &SpInterpreter::LOR_2, &SpInterpreter::EQL_2,
-                     &SpInterpreter::LSS_2},
-    m_nary_operators{&SpInterpreter::ADD_N, &SpInterpreter::LAND_N, &SpInterpreter::LOR_N} {}
- 
+            identities { context.int_val(0),
+                         context.bool_val(true),
+                         context.bool_val(false) } {}
+
   z3::expr visit(const AnyExpr<bool>& expr) {
     return context.bool_const(expr.identifier().c_str());
   }
@@ -193,8 +195,8 @@ public:
   }
 
   z3::expr visit(const UnaryExpr& expr) {
-    const UnaryOperator op = m_unary_operators[expr.op() - UNARY_BEGIN];
-    return CALL_MEMBER_FN(this, op)(expr.operand());
+    const UnaryOperator op = find_unary_operator(expr.op());
+    return op(this, expr.operand());
   }
 
   z3::expr visit(const IfThenElseExpr& expr) {
@@ -219,12 +221,13 @@ public:
     }
 
     if(operands == 2) {
-      const BinaryOperator op = m_binary_operators[expr.op() - UNARY_END];
-      return CALL_MEMBER_FN(this, op)(expr.operands().front(), expr.operands().back());
+      const BinaryOperator op = find_binary_operator(expr.op());
+      return op(this, expr.operands().front(), expr.operands().back());
     }
     
-    const NaryOperator op = m_nary_operators[expr.op() - NARY_BEGIN];
-    return CALL_MEMBER_FN(this, op)(expr.operands());
+    const NaryOperator op = find_nary_operator(expr.op());
+    const z3::expr& identity = identities[expr.op() - NARY_BEGIN];
+    return op(this, identity, expr.operands());
   }
 
 };
