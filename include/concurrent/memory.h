5f4643e976f6ec42ccbce1fd277049c0b3f53e24
Alex Horn <alex.horn@cs.ox.ac.uk>
2013-03-06 10:58:38 +0000

    Add MemoryAddr::is_shared()

--- include/concurrent/memory.h
+++ include/concurrent/memory.h
@@ -14,23 +14,27 @@ namespace se {
 class MemoryAddr {
 private:
   const std::unordered_set<uintptr_t> m_ptrs;
+  const bool m_is_shared;
 
-  MemoryAddr(std::unordered_set<uintptr_t>&& ptrs) :
-    m_ptrs(std::move(ptrs)) {}
+  MemoryAddr(std::unordered_set<uintptr_t>&& ptrs, bool is_shared) :
+    m_ptrs(std::move(ptrs)), m_is_shared(is_shared) {}
 
 public:
-  MemoryAddr(uintptr_t ptr) : m_ptrs{ ptr } {}
+  MemoryAddr(uintptr_t ptr, bool is_shared = true) :
+    m_ptrs{ ptr }, m_is_shared(is_shared) {}
 
   const std::unordered_set<uintptr_t>& ptrs() const { return m_ptrs; }
 
   static MemoryAddr join(const MemoryAddr& a, const MemoryAddr& b) {
     std::unordered_set<uintptr_t> join_ptrs(a.m_ptrs);
     join_ptrs.insert(b.m_ptrs.cbegin(), b.m_ptrs.cend());
-    return MemoryAddr(std::move(join_ptrs));
+    return MemoryAddr(std::move(join_ptrs), a.is_shared() || b.is_shared());
   }
 
+  bool is_shared() const { return m_is_shared; }
+
   bool operator==(const MemoryAddr& other) const {
-    return m_ptrs == other.m_ptrs;
+    return m_ptrs == other.m_ptrs && m_is_shared == other.m_is_shared;
   }
 };
 
--- test/concurrent/memory_test.cpp
+++ test/concurrent/memory_test.cpp
@@ -30,3 +30,43 @@ TEST(MemoryAddrTest, Join) {
   const MemoryAddr join_addr = MemoryAddr::join(addr_a, addr_b);
   EXPECT_EQ(2, join_addr.ptrs().size());
 }
+
+TEST(MemoryAddrTest, DefaultIsShared) {
+  uintptr_t ptr_a = 0x03fa;
+
+  const MemoryAddr addr(ptr_a);
+  EXPECT_TRUE(addr.is_shared());
+}
+
+TEST(MemoryAddrTest, JoinBothAreNotShared) {
+  uintptr_t ptr_a = 0x03fa;
+  uintptr_t ptr_b = 0x03fb;
+
+  const MemoryAddr addr_a(ptr_a, false);
+  const MemoryAddr addr_b(ptr_b, false);
+
+  const MemoryAddr join_addr = MemoryAddr::join(addr_a, addr_b);
+  EXPECT_FALSE(join_addr.is_shared());
+}
+
+TEST(MemoryAddrTest, JoinLeftIsShared) {
+  uintptr_t ptr_a = 0x03fa;
+  uintptr_t ptr_b = 0x03fb;
+
+  const MemoryAddr addr_a(ptr_a, true);
+  const MemoryAddr addr_b(ptr_b, false);
+
+  const MemoryAddr join_addr = MemoryAddr::join(addr_a, addr_b);
+  EXPECT_TRUE(join_addr.is_shared());
+}
+
+TEST(MemoryAddrTest, JoinRightIsShared) {
+  uintptr_t ptr_a = 0x03fa;
+  uintptr_t ptr_b = 0x03fb;
+
+  const MemoryAddr addr_a(ptr_a, false);
+  const MemoryAddr addr_b(ptr_b, true);
+
+  const MemoryAddr join_addr = MemoryAddr::join(addr_a, addr_b);
+  EXPECT_TRUE(join_addr.is_shared());
+}
