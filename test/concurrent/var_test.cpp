2c56582b1495bbaad48da12e2fa4f583be2c2864
Alex Horn <alex.horn@cs.ox.ac.uk>
2013-03-27 11:50:15 +0000

    Test initial event identifier in DeclVar<T>

--- test/concurrent/var_test.cpp
+++ test/concurrent/var_test.cpp
@@ -14,6 +14,7 @@ TEST(VarTest, SharedDeclVarInitScalar) {
   const LiteralReadInstr<char>& instr = dynamic_cast<const LiteralReadInstr<char>&>(var.direct_write_event_ref().instr_ref());
   EXPECT_EQ(0, instr.literal());
   EXPECT_TRUE(var.addr().is_shared());
+  EXPECT_EQ(2*7+1, var.direct_write_event_ref().event_id());
 }
 
 TEST(VarTest, LocalDeclVarInitScalar) {
@@ -25,4 +26,5 @@ TEST(VarTest, LocalDeclVarInitScalar) {
   const LiteralReadInstr<char>& instr = dynamic_cast<const LiteralReadInstr<char>&>(var.direct_write_event_ref().instr_ref());
   EXPECT_EQ(0, instr.literal());
   EXPECT_FALSE(var.addr().is_shared());
+  EXPECT_EQ(2*7+1, var.direct_write_event_ref().event_id());
 }
