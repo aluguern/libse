fa9bd4c73cd669cd9475ee76ed1062a9a423dc76
Alex Horn <alex.horn@cs.ox.ac.uk>
2013-04-02 15:12:42 +0100

    Add SV-COMP'13 "fib" benchmark for N=6
    The safety property is that both integer variables i and j need to be
    less than or equal to the 2*N^th Fibonacci number. We have benchmarks
    for N=5 and N=6.

--- bench/fib_006_safe_bench.cpp
+++ bench/fib_006_safe_bench.cpp
@@ -1,9 +1,9 @@
 // Adapted from the SV-COMP'13 benchmark:
-//   https://svn.sosy-lab.org/software/sv-benchmarks/trunk/c/pthread/fib_bench_safe.c
+//   https://svn.sosy-lab.org/software/sv-benchmarks/trunk/c/pthread/fib_bench_longer_safe.c
 
 #include "concurrent.h"
 
-#define NUM 5
+#define N 6
 
 int main(void) {
   se::Z3 z3;
@@ -16,18 +16,18 @@ int main(void) {
   int k;
 
   se::Threads::begin_thread(/* T0 */);
-  for (k = 0; k < NUM; k++) {
+  for (k = 0; k < N; k++) {
     i = i + j;
   }
   se::Threads::end_thread(z3);
 
   se::Threads::begin_thread(/* T1 */);
-  for (k = 0; k < NUM; k++) {
+  for (k = 0; k < N; k++) {
     j = j + i;
   }
   se::Threads::end_thread(z3);
 
-  se::Threads::error(144 < i || 144 < j, z3);
+  se::Threads::error(377 < i || 377 < j, z3);
   se::Threads::end_main_thread(z3);
 
   return z3::sat == z3.solver.check();
---
+++ bench/fib_006_unsafe_bench.cpp
@@ -0,0 +1,34 @@
+// Adapted from the SV-COMP'13 benchmark:
+//   https://svn.sosy-lab.org/software/sv-benchmarks/trunk/c/pthread/fib_bench_longer_unsafe.c
+
+#include "concurrent.h"
+
+#define N 6
+
+int main(void) {
+  se::Z3 z3;
+
+  se::Threads::reset();
+  se::Threads::begin_main_thread();
+
+  se::SharedVar<int> i = 1, j = 1;
+
+  int k;
+
+  se::Threads::begin_thread(/* T0 */);
+  for (k = 0; k < N; k++) {
+    i = i + j;
+  }
+  se::Threads::end_thread(z3);
+
+  se::Threads::begin_thread(/* T1 */);
+  for (k = 0; k < N; k++) {
+    j = j + i;
+  }
+  se::Threads::end_thread(z3);
+
+  se::Threads::error(377 < i || 377 == i || 377 < j || 377 == j, z3);
+  se::Threads::end_main_thread(z3);
+
+  return z3::unsat == z3.solver.check();
+}
