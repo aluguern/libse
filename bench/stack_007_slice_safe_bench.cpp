2b68b425d1b2d81d862601625938cedef26a74e2
Alex Horn <alex.horn@cs.ox.ac.uk>
2013-04-03 17:23:53 +0100

    Add benchmark that uses if-then statement
    This benchmark does not include the stack array as we want to measure
    its effects separately.

---
+++ bench/stack_007_slice_safe_bench.cpp
@@ -0,0 +1,53 @@
+// Adapted from the SV-COMP'13 benchmark:
+//   https://svn.sosy-lab.org/software/sv-benchmarks/trunk/c/pthread/stack_safe.c
+
+#include "libse.h"
+#include "concurrent/mutex.h"
+
+#define N 12
+
+se::SharedVar<unsigned int> top = 0;
+se::Mutex mutex;
+
+void push(int x) {
+  se::Thread::error(top == N);
+
+  top = top + 1;
+}
+
+int pop() {
+  se::Thread::error(top == 0);
+
+  top = top - 1;
+  return 0;
+}
+
+void f1() {
+  int i;
+  for (i = 0; i < N; i++) {
+    mutex.lock();
+    push(i);
+    mutex.unlock();
+  }
+}
+
+void f2() {
+  int i;
+  for (i = 0; i < N; i++) {
+    mutex.lock();
+    if (se::ThisThread::recorder().begin_then(0 < top)) {
+      pop();
+    }
+    se::ThisThread::recorder().end_branch();
+    mutex.unlock();
+  }
+}
+
+int main(void) {
+  se::Thread t1(f1);
+  se::Thread t2(f2);
+
+  se::Thread::end_main_thread();
+
+  return z3::sat == se::Thread::z3().solver.check();
+}
