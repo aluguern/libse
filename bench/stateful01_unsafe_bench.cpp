049952261e6445f96fdd66483d08d561257d8088
Alex Horn <alex.horn@cs.ox.ac.uk>
2013-04-03 16:10:12 +0100

    Add benchmark that uses Mutex and Thread::join()

---
+++ bench/stateful01_unsafe_bench.cpp
@@ -0,0 +1,42 @@
+// Adapted from the SV-COMP'13 benchmark:
+//   https://svn.sosy-lab.org/software/sv-benchmarks/trunk/c/pthread/stateful01_unsafe.c
+
+#include "libse.h"
+#include "concurrent/mutex.h"
+
+se::SharedVar<int> i = 10, j = 10;
+se::Mutex mutex;
+
+void f0() {
+  mutex.lock();
+  i = i + 1;
+  mutex.unlock();
+
+  mutex.lock();
+  j = j + 1;
+  mutex.unlock();
+}
+
+void f1() {
+  mutex.lock();
+  i = i + 5;
+  mutex.unlock();
+
+  mutex.lock();
+  j = j - 6;
+  mutex.unlock();
+}
+
+int main(void) {
+  se::Thread t0(f0);
+  se::Thread t1(f1);
+
+  t0.join();
+  t1.join();
+
+  se::Thread::error(i == 16 && j == 5);
+
+  se::Thread::end_main_thread();
+
+  return z3::unsat == se::Thread::z3().solver.check();
+}
